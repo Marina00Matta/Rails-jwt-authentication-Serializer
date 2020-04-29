d84785b22a12236dafb961ad6453153e9e6985c3
Marina00Matta <marinamedhat_19@hotmail.com>
2020-04-29 23:47:42 +0200

    serializer gem

--- Gemfile
+++ Gemfile
@@ -45,3 +45,5 @@ gem 'bcrypt', '~> 3.1.7'
 gem 'jwt'
 
 gem 'simple_command'
+
+gem 'active_model_serializers'
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  resources :posts
   resources :items
   post 'authenticate', to: 'authentication#authenticate'
 end
